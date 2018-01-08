435b72b2eeadae560f062bb23dd8da9b0feec9b7
Matt Hayman <mhayman@ucar.edu>
2018-01-08 13:44:46 -0700

    remove instances where the data system is reporting nans (-32767) from aircraft data set.

--- libraries/GVHSRLlib.py
+++ libraries/GVHSRLlib.py
@@ -282,7 +282,8 @@ def load_aircraft_data(filename,var_list):
 
     var_data = dict(zip(var_list,[np.array([])]*len(var_list)))
     f = nc4.Dataset(filename,'r')
-            
+    
+    i_nan = []  # list of invalid values from the data system     
     for var in var_data.keys():
         if any(var in s for s in f.variables):
             data = lp.ncvar(f,var)
@@ -290,6 +291,12 @@ def load_aircraft_data(filename,var_list):
                 var_data[var] = np.concatenate((var_data[var],data)) 
             else:
                 var_data[var] = data.copy()
+            i_nan.extend(list(np.nonzero(var_data[var]==-32767)[0]))
+    i_nan = np.unique(np.array(i_nan))  # force list to be unique
+    
+    # delete data where nans are present
+    for var in var_data.keys():
+        var_data[var] = np.delete(var_data[var],i_nan)
                 
     print('Aircraft Time Data: %s' %f.variables['Time'].units)
     f.close()
