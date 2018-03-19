Fix for time_stop calculation during full flight processing.

processors/Airborne_GVHSRL_DataSelection.py
@@ -257,7 +257,7 @@ def SelectAirborneData(settings={},paths={},process_vars={}):
     if settings['full_flight']:
         if settings['use_aircraft_tref']:
             time_start = aircraft_t_ref+datetime.timedelta(seconds=np.int(air_data['Time'][it0]))
-            time_stop = time_start+datetime.timedelta(seconds=np.int(air_data['Time'][it1]))
+            time_stop = aircraft_t_ref+datetime.timedelta(seconds=np.int(air_data['Time'][it1]))
         else:
             time_start = flight_date[usr_flt]+datetime.timedelta(seconds=np.int(air_data['Time'][it0]))
             time_stop = flight_date[usr_flt]+datetime.timedelta(seconds=np.int(air_data['Time'][it1]))